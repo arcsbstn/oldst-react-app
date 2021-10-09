8ebb3824a6426ba670762b77e90c577281449bc4
ARC Sebastian <arcsbstn@gmail.com>
2021-10-09 21:45:05 +0800

    add styling for Ad

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -1,12 +1,15 @@
+import '../styles/Ad.scss'
+
 export default function Ad({ info }) {
   const { url } = info;
   return (
-    <>
+    <div className='Ad__placement'>
+      <p>SPONSORED MATERIAL</p>
       <img
         className='Ad__image'
         src={url}
         alt='ad'
       />
-    </>
+    </div>
   )
 }
