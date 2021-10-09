db874d469c8d2f10a98f219b94b344680cca233c
ARC Sebastian <arcsbstn@gmail.com>
2021-10-09 21:45:44 +0800

    update styling for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,16 @@ import '../styles/Header.scss';
 export default function Header({ sortByPrice, sortBySize, sortById }) {
   return (
     <header>
-      <div className='Header__logo'>Products Grid</div>
-      <div className='Header__sortOptions'>
-        <span>Sort by:</span>
-        <ul>
-          <li><button onClick={sortByPrice}>Price</button></li>
-          <li><button onClick={sortBySize}>Size</button></li>
-          <li><button onClick={sortById}>ID</button></li>
-        </ul>
+      <div className='Header__wrapper'>
+        <div className='Header__logo'>Products Grid</div>
+        <div className='Header__sortOptions'>
+          <span>Sort by:</span>
+          <ul>
+            <li><button onClick={sortByPrice}>Price</button></li>
+            <li><button onClick={sortBySize}>Size</button></li>
+            <li><button onClick={sortById}>ID</button></li>
+          </ul>
+        </div>
       </div>
     </header>
   )
