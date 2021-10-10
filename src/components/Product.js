328f5e6a8e516c444935000dbcc449a5185b4a21
ARC Sebastian <arcsbstn@gmail.com>
2021-10-10 15:58:52 +0800

    format price: cents -> dollars

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,14 @@
 import '../styles/Product.scss'
 
 export default function Product({ info }) {
-  const { size, price, face, date } = info;
+  let { size, price, face, date } = info;
+  let priceInDollars = (Math.round(price) / 100).toFixed(2);
 
   return (
     <>
-      <span className='Product__face' style={{fontSize: `${size}px`}}>{face}</span>
+      <span className='Product__face' style={{ fontSize: `${size}px` }}>{face}</span>
       <div className='Product__displayInfo'>
-        <p>${price}</p>
+        <p className='price'>${priceInDollars}</p>
         <p>{size}</p>
         <p>{date}</p>
       </div>
