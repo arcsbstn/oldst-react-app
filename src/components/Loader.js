1ccf16015e2b8a5ba8de291c951880eee1b3fdba
ARC Sebastian <arcsbstn@gmail.com>
2021-10-10 15:28:04 +0800

    use ellipsis loading animation

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import '../styles/Loader.scss';
 export default function Loader() {
   return (
     <div className='Loader__wrapper'>
-      <div className='lds-ring'>
+      <div className='lds-ellipsis'>
         <div></div>
         <div></div>
         <div></div>
