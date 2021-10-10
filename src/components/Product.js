b8fa70c0f6053e422d8741faa9f8690cf050b39b
ARC Sebastian <arcsbstn@gmail.com>
2021-10-10 20:58:56 +0800

    update size and post date formats

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,24 @@
+import moment from 'moment'
+
 import '../styles/Product.scss'
 
 export default function Product({ info }) {
-  let { size, price, face, date } = info;
-  let priceInDollars = (Math.round(price) / 100).toFixed(2);
+  const { size, price, face, date } = info;
+
+  const priceInDollars = (Math.round(price) / 100).toFixed(2);
+  const today = moment();
+  const oneWeekFromToday = today.clone().subtract(7, 'days').startOf('day');
+  const isWithinAWeek = moment(date).isAfter(oneWeekFromToday);
+  const relativeDate = moment(date.substr(3, 12)).fromNow()
+  const fullDate = date.substr(3, 12)
 
   return (
     <>
       <span className='Product__face' style={{ fontSize: `${size}px` }}>{face}</span>
       <div className='Product__displayInfo'>
-        <p className='price'>${priceInDollars}</p>
-        <p>{size}</p>
-        <p>{date}</p>
+        <p className='Product__price'>${priceInDollars}</p>
+        <p>Size: {size}px</p>
+        <p>Posted {isWithinAWeek ? relativeDate : fullDate}</p>
       </div>
     </>
   )
